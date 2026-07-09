9b6577b279e881d8ac5e65f29c12c26008c4671c
kristall268 <kristall268@outlook.com>
2026-07-09 15:44:05 +0200

    Add plasma-manager to flake.nix inputs

--- flake.nix
+++ flake.nix
@@ -8,9 +8,14 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, plasma-manager, ... }:
   let
     system = "x86_64-linux";
   in {
@@ -22,6 +27,9 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.sharedModules = [
+            plasma-manager.homeManagerModules.plasma-manager
+          ];
           home-manager.users.krist = import ./home.nix;
         }
       ];
